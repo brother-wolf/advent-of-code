b9e6a75d3c2c24170471a5393b263977ab5534fb
brother-wolf <richard@brotherwolf.org>
2019-12-29 18:56:56 +0000

    D3P1|D3P2|RL: Cleaning up imports

--- src/day3/day3-part1.rs
+++ src/day3/day3-part1.rs
@@ -1,4 +1,3 @@
-use advent_of_code_2019_rust::day3::geometry::Point;
 use advent_of_code_2019_rust::helpers::files::read_file;
 use advent_of_code_2019_rust::day3::generators::{build_path_from, find_intersection_points, Intersection};
 
@@ -34,6 +33,7 @@ fn process(input_1: Vec<&str>, input_2: Vec<&str>) -> Option<Intersection> {
 
 #[test]
 fn example_one_process() {
+    use advent_of_code_2019_rust::day3::geometry::Point;
     let expected = Point {x:3, y:3};
 
     let wire_1 = vec!["R8","U5","L5","D3"];
--- src/day3/day3-part2.rs
+++ src/day3/day3-part2.rs
@@ -1,4 +1,3 @@
-use advent_of_code_2019_rust::day3::geometry::Point;
 use advent_of_code_2019_rust::helpers::files::read_file;
 use advent_of_code_2019_rust::day3::generators::{build_path_from, find_intersection_points, Intersection};
 
@@ -35,6 +34,7 @@ fn process(input_1: Vec<&str>, input_2: Vec<&str>) -> Option<Intersection> {
 
 #[test]
 fn example_one_process() {
+    use advent_of_code_2019_rust::day3::geometry::Point;
     let expected = Intersection { steps: 30, point: Point {x:6, y:5} };
     let wire_1 = vec!["R8","U5","L5","D3"];
     let wire_2 = vec!["U7","R6","D4","L4"];
